9c7eef8d199b9ac748da0dceb94e966b8633aabd
Leo-d-D <leod91791@gmail.com>
2024-10-27 16:10:17 -0700

    Add files via upload

---
+++ DEGs/DEG_limma_trend_subclass.r
@@ -0,0 +1,78 @@
+library(reticulate)
+scipy_sparse <- import("scipy.sparse")
+args = commandArgs(trailingOnly=TRUE)
+
+gene_file <- args[1]
+cell_file <- args[2]
+count_file <- args[3]
+
+run_limmatrend<-function(processed,cellinfo,cov=T,former.meth=''){
+  library(limma)
+  library(edgeR)
+  library(dplyr)
+  library(magrittr)
+  count_df<-processed
+  rownames(cellinfo)=cellinfo$cell_id
+  cellinfo<-cellinfo[colnames(processed),]
+  #cellinfo$subclass%<>%factor()
+  #cellinfo$batch%<>%factor()
+ 
+  cellinfo.cov<-cellinfo[,c('subclass','batch')]
+  ## Convert to an edgeR object
+  dgeObj <- DGEList(count_df)
+  ## Perform TMM normalisation
+  dgeObj <- calcNormFactors(dgeObj)
+  logCPM <- cpm(dgeObj, log=TRUE, prior.count=3)
+  if(cov){
+    design<-model.matrix(formula(paste(c("~ 0 +  subclass", setdiff(colnames(cellinfo.cov),c('subclass'))), collapse = '+')), data=cellinfo.cov)
+  }else{
+    design <- model.matrix(~ subclass, data=cellinfo.cov)
+  }
+  subclass_list <- unique(cellinfo.cov$subclass)
+  for (i in subclass_list) {
+    temp_cellinfo <- cellinfo.cov
+    print(i)
+    temp_cellinfo$subclass[temp_cellinfo$subclass != i] <- "other"
+    temp_cellinfo$subclass%<>%factor()
+    temp_cellinfo$batch%<>%factor()
+    
+    #temp_cellinfo %<>% mutate(subclass = replace(subclass, subclass != i, "other"))
+    print(levels(temp_cellinfo$subclass))
+    design <- model.matrix(formula(paste(c("~ 0 +  subclass", setdiff(colnames(temp_cellinfo),c('subclass'))), collapse = '+')), data=temp_cellinfo)
+    print(design[1,])
+    constrasts <- makeContrasts(contrasts=paste(paste0("subclass",i),"-",paste0("subclass","other"),sep=""),levels = design)
+    lmfit <- lmFit(logCPM, design)
+    fitcontrasts <- contrasts.fit(lmfit,constrasts)
+    #lmfit <- eBayes(lmfit, trend=TRUE, robust = TRUE)
+    fitcontrasts2 <- eBayes(fitcontrasts,trend=TRUE, robust = TRUE)
+    res_table <- topTable(fitcontrasts2, n = Inf, adjust.method = "BH")
+    res_name<-paste0(ifelse(former.meth=='','',paste0(former.meth,'+')),i ,'_limmatrend',ifelse(cov,'_Cov',''))
+    save(res_table, temp_cellinfo, file=paste0(res_name,'.rda'))
+    write.table(res_table,paste0(res_name, ".tsv"),quote=F,sep="\t")
+  }
+  #constrasts <- makeContrasts(subclassGABAergic - subclassGlutamatergic, subclassGABAergic - subclassNon_Neuronal, subclassGlutamatergic - subclassNon_Neuronal,levels = design)
+
+  #lmfit <- lmFit(logCPM, design)
+  #fitcontrasts <- contrasts.fit(lmfit,constrasts)
+  #lmfit <- eBayes(lmfit, trend=TRUE, robust = TRUE)
+  #fitcontrasts2 <- eBayes(fitcontrasts,trend=TRUE, robust = TRUE)
+  #res_table <- topTable(fitcontrasts2, n = Inf, adjust.method = "BH", coef = 2)
+  #res <- data.frame('pvalue' = res_table$P.Value, 
+  #                           'adjpvalue' = res_table$adj.P.Val, 
+  #                           'logFC' = res_table$logFC,
+  #                           row.names = rownames(res_table))
+
+  #res_name<-paste0(ifelse(former.meth=='','',paste0(former.meth,'+')),'limmatrend',ifelse(cov,'_Cov',''))
+  #save(res, cellinfo, file=paste0('./',res_name,'.rda'))
+  #write.table(res_table,"res.txt",quote=F,sep="\t")
+  #return(res_name)
+}
+
+counts <- scipy_sparse$load_npz(count_file)
+counts_df <- as.data.frame(t(as.matrix(counts)))
+cellinfo <- read.table(cell_file, sep="\t", header = T)
+colnames(counts_df) <- cellinfo$cell_id
+genes <- read.table(gene_file,sep="\t",header=T)
+colnames(counts_df) <- cellinfo$cell_id
+rownames(counts_df) <- genes$gene
+run_limmatrend(counts_df, cellinfo)
